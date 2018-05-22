dcbf6270d2b250da87388f0822b28d9d2b52e5a2
wollyee <douglas.infa@gmail.com>
2018-05-22 10:36:32 -0700

    Update dashboard

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html class="route-documentation fontawesome-i2svg-active fontawesome-i2svg-complete">
 
 <head>
     <meta charset="utf-8">
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <!-- Bulma Version 0.7.1-->
-    <link rel="stylesheet" href="./css/bulma.min.css" />
+    <link rel="stylesheet" href="./css/bulma.css" />
     <!--<link rel="stylesheet" type="text/css" href="../css/login.css">-->
 </head>
     <body style="background: #ecf0f5;">
