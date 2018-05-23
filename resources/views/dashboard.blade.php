88151447610a06f8d954bfcf7f0eb07294af39f8
wollyee <douglas.infa@gmail.com>
2018-05-23 07:28:58 -0700

    Update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,8 @@
 @include('includes.header')       
 
 <style>
-	.navtop{color:#038672!important;}
-	.navtop:hover{color:#038672!important;}
+	.navtop{color:#038672;}
+	.navtop:hover{color:#055044;}
 </style>
 
 <div class="columns" style="font-weight:100;">
@@ -22,7 +22,7 @@
 			        		</div>
 
 			        		<div style="float:right; width:120px; height:20px; text-align: center; margin:0px 10px;">
-			        			<a class="navtop" href="{{url('/')}}"><i class="fa fa-bell" style="margin-right:5px; font-size:28px;"></i><br />Notificações (0)</a>
+			        			<a class="navtop" href="{{url('/')}}"><i class="fa fa-bell" style="margin-right:5px; font-size:28px;"></i><br />Notificações <span style="padding:2px 6px; background:#ff3860; font-size:11px; color:#FFF; font-weight:bold; border-radius:5px;">7</span></a>
 			        		</div>
 
 			        		<div style="float:right; width:90px; height:20px; text-align: center; margin:0px 25px;">
@@ -36,36 +36,26 @@
 		</div>
 
 		<div class="tile is-ancestor" style="text-align:center; height:200px;">
-		  <div class="tile is-vertical is-12">
-		    <div class="tile">
-		      <div class="tile is-parent is-vertical">
-		        <article class="tile is-child notification" style="background:#d2e6e3; border-radius:0px;">
-		          <p class="title is-2">Dashboard</p>
-		          <p class="subtitle">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
-		        </article>
-		      </div>
-		    </div>
-		  </div>
+			<div class="tile is-vertical is-12">
+			    <div class="tile">
+			      	<div class="tile is-parent is-vertical">
+			        	<article class="tile is-child notification" style="background:#d2e6e3; border-radius:0px;">
+			          		<p class="title is-2">Dashboard</p>
+			          		<p class="subtitle">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos.</p>
+			        	</article>
+			      	</div>
+			    </div>
+			</div>
 		</div>
 
 		<div class="column is-11" style="width:98%; height:588px; background:#FFF; margin:0 auto; margin-top:-80px; z-index:99999; position:relative;">
 
 			<div class="columns">
 				<div class="column" style="text-align:left;">
-				    <div class="notification is-primary" style="line-height:25px; font-weight:100!important;">
+				    <div class="notification is-info" style="line-height:25px; font-weight:100!important;">
 				    	<i class="fa fa-street-view" style="float:left; font-size:56px;font-weight:100!important;"></i>
 				    	<div style="float:left; font-size:20px; font-weight:bold; margin-top:3px; margin-left:25px;">
-				    		<span style="font-size:35px;">768</span><br />
-				    		<span style="font-weight:100!important;">Pedidos realizados</span>
-				    	</div>
-				    	<div style="clear:both;"></div>
-				    </div>
-				</div>
-				<div class="column" style="text-align:left;">
-				    <div class="notification is-link" style="line-height:25px; font-weight:100!important;">
-				    	<i class="fa fa-gear" style="float:left; font-size:56px;font-weight:100!important;"></i>
-				    	<div style="float:left; font-size:20px; font-weight:bold; margin-top:3px; margin-left:25px;">
-				    		<span style="font-size:35px;">768</span><br />
+				    		<span style="font-size:35px;">1033</span><br />
 				    		<span style="font-weight:100!important;">Pedidos realizados</span>
 				    	</div>
 				    	<div style="clear:both;"></div>
@@ -73,10 +63,20 @@
 				</div>
 				<div class="column" style="text-align:left;">
 				    <div class="notification is-info" style="line-height:25px; font-weight:100!important;">
+				    	<i class="fa fa-gear" style="float:left; font-size:56px;font-weight:100!important;"></i>
+				    	<div style="float:left; font-size:20px; font-weight:bold; margin-top:3px; margin-left:25px;">
+				    		<span style="font-size:35px;">320</span><br />
+				    		<span style="font-weight:100!important;">Impressas</span>
+				    	</div>
+				    	<div style="clear:both;"></div>
+				    </div>
+				</div>
+				<div class="column" style="text-align:left;">
+				    <div class="notification is-primary" style="line-height:25px; font-weight:100!important;">
 				    	<i class="fa fa-users" style="float:left; font-size:56px;font-weight:100!important;"></i>
 				    	<div style="float:left; font-size:20px; font-weight:bold; margin-top:3px; margin-left:25px;">
-				    		<span style="font-size:35px;">768</span><br />
-				    		<span style="font-weight:100!important;">Pedidos realizados</span>
+				    		<span style="font-size:35px;">159</span><br />
+				    		<span style="font-weight:100!important;">Recebidas</span>
 				    	</div>
 				    	<div style="clear:both;"></div>
 				    </div>
@@ -85,8 +85,8 @@
 				    <div class="notification is-danger" style="line-height:25px; font-weight:100!important;">
 				    	<i class="fa fa-list-alt" style="float:left; font-size:56px;font-weight:100!important;"></i>
 				    	<div style="float:left; font-size:20px; font-weight:bold; margin-top:3px; margin-left:25px;">
-				    		<span style="font-size:35px;">768</span><br />
-				    		<span style="font-weight:100!important;">Pedidos realizados</span>
+				    		<span style="font-size:35px;">26</span><br />
+				    		<span style="font-weight:100!important;">Não impressa</span>
 				    	</div>
 				    	<div style="clear:both;"></div>
 				    </div>
@@ -96,142 +96,68 @@
 			<div class="columns" style="text-align:left;">
 				
 				<div class="column">
-					<div class="chart-container">
-						<canvas id="chart-legend-normal"></canvas>
-					</div>
+		          	<p class="subtitle" style="font-weight:100;">Últimos pedidos</p>
+		          	<table class="table" style="width:100%;">
+					  <thead>
+					    <tr>
+					      <th>Data</th>
+					      <th>Tipo</th>
+					      <th>UF</th>
+					      <th>Cidade</th>
+					      <th style="text-align:center;">Ações</th>
+					    </tr>
+					  </thead>
+					  <tbody>
+					    <tr>
+					      <th>23/05/2018</th>
+					      <td>Nascimento</td>
+					      <td>SP</td>
+					      <td>São Paulo</td>
+					      <td style="text-align:center;"><button class="button is-primary">Visualizar</button> <button class="button is-danger">Notificar</button></td>
+					    </tr>
+					    <tr>
+					      <th>22/05/2018</th>
+					      <td>Casamento</td>
+					      <td>SP</td>
+					      <td>São Paulo</td>
+					      <td style="text-align:center;"><button class="button is-primary">Visualizar</button> <button class="button is-danger">Notificar</button></td>
+					    </tr>
+					    <tr>
+					      <th>21/05/2018</th>
+					      <td>Óbito</td>
+					      <td>SP</td>
+					      <td>São Paulo</td>
+					      <td style="text-align:center;"><button class="button is-primary">Visualizar</button> <button class="button is-danger">Notificar</button></td>
+					    </tr>
+					    <tr>
+					      <th>20/05/2018</th>
+					      <td>Casamento</td>
+					      <td>SP</td>
+					      <td>São Paulo</td>
+					      <td style="text-align:center;"><button class="button is-primary">Visualizar</button> <button class="button is-danger">Notificar</button></td>
+					    </tr>
+					    <tr>
+					      <th>19/05/2018</th>
+					      <td>Nascimento</td>
+					      <td>SP</td>
+					      <td>São Paulo</td>
+					      <td style="text-align:center;"><button class="button is-primary">Visualizar</button> <button class="button is-danger">Notificar</button></td>
+					    </tr>
+					    <tr>
+					      <th>18/05/2018</th>
+					      <td>Óbito</td>
+					      <td>SP</td>
+					      <td>São Paulo</td>
+					      <td style="text-align:center;"><button class="button is-primary">Visualizar</button> <button class="button is-danger">Notificar</button></td>
+					    </tr>				    
+					  </tbody>
+					</table>
 				</div>
-				
-				<div class="column">
-					<div class="notification" style="line-height:25px; font-weight:100!important;">
-				    	<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-					        <thead>
-						        <tr>
-						          <th>One</th>
-						          <th>Two</th>
-						        </tr>
-					        </thead>
-					        <tbody>
-						        <tr>
-						          <td>Three</td>
-						          <td>Four</td>
-						        </tr>
-						        <tr>
-						          <td>Five</td>
-						          <td>Six</td>
-						        </tr>
-						        <tr>
-						          <td>Seven</td>
-						          <td>Eight</td>
-						        </tr>
-						        <tr>
-						          <td>Nine</td>
-						          <td>Ten</td>
-						        </tr>
-						        <tr>
-						          <td>Eleven</td>
-						          <td>Twelve</td>
-						        </tr>
-						        <tr>
-						          <td>Eleven</td>
-						          <td>Twelve</td>
-						        </tr>
-						        <tr>
-						          <td>Eleven</td>
-						          <td>Twelve</td>
-						        </tr>
-					        </tbody>
-					    </table>
-				    </div>
-				</div>
-				
-			</div>
 
-			<div class="columns" style="text-align:left;">
-				
 				<div class="column">
-					<div class="notification" style="line-height:25px; font-weight:100!important;">
-				    	<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-					        <thead>
-						        <tr>
-						          <th>One</th>
-						          <th>Two</th>
-						        </tr>
-					        </thead>
-					        <tbody>
-						        <tr>
-						          <td>Three</td>
-						          <td>Four</td>
-						        </tr>
-						        <tr>
-						          <td>Five</td>
-						          <td>Six</td>
-						        </tr>
-						        <tr>
-						          <td>Seven</td>
-						          <td>Eight</td>
-						        </tr>
-						        <tr>
-						          <td>Nine</td>
-						          <td>Ten</td>
-						        </tr>
-						        <tr>
-						          <td>Eleven</td>
-						          <td>Twelve</td>
-						        </tr>
-						        <tr>
-						          <td>Eleven</td>
-						          <td>Twelve</td>
-						        </tr>
-						        <tr>
-						          <td>Eleven</td>
-						          <td>Twelve</td>
-						        </tr>
-					        </tbody>
-					    </table>
-				    </div>
-				</div>
-				
-				<div class="column">
-					<div class="notification" style="line-height:25px; font-weight:100!important;">
-				    	<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-					        <thead>
-						        <tr>
-						          <th>One</th>
-						          <th>Two</th>
-						        </tr>
-					        </thead>
-					        <tbody>
-						        <tr>
-						          <td>Three</td>
-						          <td>Four</td>
-						        </tr>
-						        <tr>
-						          <td>Five</td>
-						          <td>Six</td>
-						        </tr>
-						        <tr>
-						          <td>Seven</td>
-						          <td>Eight</td>
-						        </tr>
-						        <tr>
-						          <td>Nine</td>
-						          <td>Ten</td>
-						        </tr>
-						        <tr>
-						          <td>Eleven</td>
-						          <td>Twelve</td>
-						        </tr>
-						        <tr>
-						          <td>Eleven</td>
-						          <td>Twelve</td>
-						        </tr>
-						        <tr>
-						          <td>Eleven</td>
-						          <td>Twelve</td>
-						        </tr>
-					        </tbody>
-					    </table>
-				    </div>
+					<div class="chart-container">
+						<canvas id="canvas"></canvas>
+					</div>
 				</div>
 				
 			</div>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,35 +1,35 @@
 @include('includes.header')       
 
     <section class="is-success is-fullheight">
-            <div class="hero-body">
-                <div class="container has-text-centered">
-                    <div class="column is-4 is-offset-4">
-                        <h3 class="title has-text-grey">Login</h3>
-                        <div class="box">
-                            <figure class="avatar">
-                                <div style="border-radius: 50%; width:128px; height:128px; background:#CCC; margin:0 auto;">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo0d-gG63jqXDLy-HBFbuDUHQfJoMXskG8_zKSqY9PuscFYqdw3Q" width="128" height="128" style="border-radius: 50%;"/>
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Login</h3>
+                    <div class="box">
+                        <figure class="avatar">
+                            <div style="border-radius: 50%; width:128px; height:128px; background:#CCC; margin:0 auto;">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo0d-gG63jqXDLy-HBFbuDUHQfJoMXskG8_zKSqY9PuscFYqdw3Q" width="128" height="128" style="border-radius: 50%;"/>
+                            </div>
+                        </figure>
+                        <br />
+                        <form>
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-medium" type="email" placeholder="Your Email" autofocus="">
                                 </div>
-                            </figure>
-                            <br />
-                            <form>
-                                <div class="field">
-                                    <div class="control">
-                                        <input class="input is-medium" type="email" placeholder="Your Email" autofocus="">
-                                    </div>
-                                </div>
+                            </div>
 
-                                <div class="field">
-                                    <div class="control">
-                                        <input class="input is-medium" type="password" placeholder="Your Password">
-                                    </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-medium" type="password" placeholder="Your Password">
                                 </div>
-                                <a href="{{url('/dashboard')}}" class="button is-block is-info is-medium is-fullwidth">Login</a>
-                            </form>
-                        </div>
+                            </div>
+                            <a href="{{url('/dashboard')}}" class="button is-block is-info is-medium is-fullwidth">Login</a>
+                        </form>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
 @include('includes.footer')  
